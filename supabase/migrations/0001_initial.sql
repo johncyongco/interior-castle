1f9283cf8ad968a43f2f034d94d16c6a8fecb9ab
johncyongco <johncyongco@users.noreply.github.com>
2026-05-13 13:49:26 +0800

    Update Supabase config

--- supabase/migrations/0001_initial.sql
+++ supabase/migrations/0001_initial.sql
@@ -1,4 +1,4 @@
-create extension if not exists "pgcrypto";
+create extension if not exists pgcrypto;
 
 create table if not exists public.users (
   id uuid primary key references auth.users (id) on delete cascade,
